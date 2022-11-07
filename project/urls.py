0e2d20dbe0e801b99e0e885f402fa2e010cf4f45
dzdn <jdustinwilliams@gmail.com>
2022-11-07 15:15:25 -0500

    Messed up tree, recomitting several pieces.

--- project/urls.py
+++ project/urls.py
@@ -17,13 +17,17 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+from homeschool.schools.views import app
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("office/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path("app/", app, name="app"),
 ]
 
 if settings.DEBUG and settings.DEBUG_TOOLBAR:
     import debug_toolbar
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-    urlpatterns = [path('__debug__', include(debug_toolbar.urls))] + urlpatterns
+    urlpatterns = [path("__debug__", include(debug_toolbar.urls))] + urlpatterns
     urlpatterns += staticfiles_urlpatterns()
